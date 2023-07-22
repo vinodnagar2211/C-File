00fdcb87e483aed01e21630396102918a692b6ec
vinodnagar2211 <vinodnagar2211@gmail.com>
2023-07-22 11:33:00 +0530

    uplod

---
+++ 22-07-2023/1  22  333.c
@@ -0,0 +1,16 @@
+ //              1  22  333 
+
+#include <stdio.h>
+int main()
+{
+    for (int i=1;i<=3;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            printf("%d",i);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}n
---
+++ 22-07-2023/3 Line and 3 Star print  --Tringle--.c
@@ -0,0 +1,16 @@
+ //               3 Line and 3 Star print  --Tringle--
+
+#include <stdio.h>
+int main()
+{
+    for (int i=1;i<=3;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {3 Line and 3 Star print  --Tringle--
+            printf("* ");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
